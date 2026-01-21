3783508b55ade5d9af854d302a5adbacce6f1a40
jianming <yeojm235@gmail.com>
2026-01-21 14:44:56 +0800

    Add list functionality

--- src/main/java/Epsilon.java
+++ src/main/java/Epsilon.java
@@ -1,16 +1,31 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Epsilon {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<>();
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Epsilon\nWhat can I do for you?\n");
         System.out.println("____________________________________________________________");
         String cmd = sc.nextLine();
         while (!cmd.equals("bye")) {
-            System.out.println("____________________________________________________________");
-            System.out.println(cmd + "\n");
-            System.out.println("____________________________________________________________");
+            if (cmd.equals("")) {
+                System.out.println("____________________________________________________________");
+                System.out.println("No Input Detected :(");
+                System.out.println("____________________________________________________________");
+            } else if (cmd.equals("list")) {
+                System.out.println("____________________________________________________________");
+                for (int i = 0; i < list.size(); i++) {
+                    System.out.println((i + 1) + ". " + list.get(i));
+                }
+                System.out.println("____________________________________________________________");
+            } else {
+                list.add(cmd);
+                System.out.println("____________________________________________________________");
+                System.out.println("added: " + cmd + "\n");
+                System.out.println("____________________________________________________________");
+            }
             cmd = sc.nextLine();
         }
         System.out.println("____________________________________________________________");
