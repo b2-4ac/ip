639f36c4706e3d069d157f89e1049f3b654bf8c8
jianming <yeojm235@gmail.com>
2026-01-21 14:31:21 +0800

    Add echo feature

--- src/main/java/Epsilon.java
+++ src/main/java/Epsilon.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
+
 public class Epsilon {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Epsilon\nWhat can I do for you?");
+        System.out.println("Hello! I'm Epsilon\nWhat can I do for you?\n");
+        System.out.println("____________________________________________________________");
+        String cmd = sc.nextLine();
+        while (!cmd.equals("bye")) {
+            System.out.println("____________________________________________________________");
+            System.out.println(cmd + "\n");
+            System.out.println("____________________________________________________________");
+            cmd = sc.nextLine();
+        }
         System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
