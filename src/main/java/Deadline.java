c4d710f17e5cc3b87f81ba475fa9c0a07a446ee0
jianming <yeojm235@gmail.com>
2026-01-27 02:08:10 +0800

    Modify class files to use java LocalDate

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,26 +1,30 @@
-public class Deadline extends Task{
-    private String deadline;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
-    public Deadline(String title, String deadline) throws MissingInputException{
+public class Deadline extends Task{
+    private LocalDate deadline;
+
+    public Deadline(String title, String deadline) throws MissingInputException, DateTimeParseException{
         super(title);
         if (deadline.trim().equals("")) {
             throw new MissingInputException();
         }
-        this.deadline = deadline.trim();
+        this.deadline = LocalDate.parse(deadline.trim());
     }
 
     @Override
     public String encode() {
         if (super.getIsCompleted()) {
-            return "D | " + "1" + " | " + super.getTitle() + " | " + this.deadline;
+            return "D | " + "1" + " | " + super.getTitle() + " | " + this.deadline.format(DateTimeFormatter.ISO_LOCAL_DATE);
         } else {
-            return "D | " + "0" + " | " + super.getTitle() + " | " + this.deadline;
+            return "D | " + "0" + " | " + super.getTitle() + " | " + this.deadline.format(DateTimeFormatter.ISO_LOCAL_DATE);
         }
     }
 
 
     @Override
     public String toString() {
-        return "/D/" + super.toString() + " (by: " + this.deadline + ")";
+        return "/D/" + super.toString() + " (by: " + this.deadline.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,28 +1,31 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
-    private String start;
-    private String end;
+    private LocalDate start;
+    private LocalDate end;
 
     public Event(String title, String start, String end) throws MissingInputException {
         super(title);
         if (start.trim().equals("") || end.trim().equals("")) {
             throw new MissingInputException();
         }
-        this.start = start;
-        this.end = end;
+        this.start = LocalDate.parse(start.trim());
+        this.end = LocalDate.parse(end.trim());
     }
 
     @Override
     public String encode() {
         if (super.getIsCompleted()) {
-            return "E | " + "1" + " | " + super.getTitle() + " | " + this.start + " | " + this.end;
+            return "E | " + "1" + " | " + super.getTitle() + " | " + this.start.format(DateTimeFormatter.ISO_LOCAL_DATE) + " | " + this.end.format(DateTimeFormatter.ISO_LOCAL_DATE);
         } else {
-            return "E | " + "0" + " | " + super.getTitle() + " | " + this.start + " | " + this.end;
+            return "E | " + "0" + " | " + super.getTitle() + " | " + this.start.format(DateTimeFormatter.ISO_LOCAL_DATE) + " | " + this.end.format(DateTimeFormatter.ISO_LOCAL_DATE);
         }
     }
 
 
     @Override
     public String toString() {
-        return "/E/" + super.toString() + " (from: " + start + " to: " + end + ")";
+        return "/E/" + super.toString() + " (from: " + this.start.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + " to: " + this.end.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
     }
 }
